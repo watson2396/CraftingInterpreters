5655a74c877b77d90bce9367079ed1c8707aab22
Jacob Watson <JacobWatson@sbp.com>
2023-10-24 09:11:31 -0400

    first commit; partial scanning chapter

---
+++ csharp-lox/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace csharp_lox;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 1) {
+            Console.WriteLine("Usage: c-sharp_lox [script]");
+        } else if (args.Length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(string filePath) {
+        byte[] bytes = File.ReadAllBytes(Path.GetFullPath(filePath));
+        Task.Run(() => System.Text.Encoding.Default.GetString(bytes));
+    }
+
+    private static void runPrompt() {
+        for (;;) {
+            Console.WriteLine("> ");
+            string? line = Console.ReadLine();
+            if (line == null) break;
+            run(line);
+        }
+
+    }
+
+    private static void run(string source) {
+
+    }
+
+}
---
+++ csharp-lox/README.md
@@ -0,0 +1,10 @@
+
+## Project setup
+
+1. create app `dotnet new console --framework net7.0 --use-program-main true`
+2. build app `dotnet build`
+3. run app `dotnet run`
+
+
+
+
