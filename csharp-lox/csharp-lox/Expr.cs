aa1367e7a5f8155274e4408250bf0d5f04b30fe7
jwatson <jwatson2396@gmail.com>
2025-04-10 21:13:18 -0400

    working on the visitor stuff still

--- csharp-lox/csharp-lox/Expr.cs
+++ csharp-lox/csharp-lox/Expr.cs
@@ -1,29 +1,24 @@
 namespace csharp_lox;
 
-public interface Visitor<T>
-{
-    T VisitBinaryExpr(Binary expr);
-    T VisitGroupingExpr(Grouping expr);
-    T VisitLiteralExpr(Literal expr);
-    T VisitUnaryExpr(Unary expr);
+public interface Visitor {
+    void VisitBinaryExpr(Binary expr);
+    void VisitGroupingExpr(Grouping expr);
+    void VisitLiteralExpr(Literal expr);
+    void VisitUnaryExpr(Unary expr);
 }
 
-public abstract class Expr
-{
-    public abstract void Accept(Visitor<Expr> visitor);
+public abstract class Expr {
+    public abstract void Accept(Visitor visitor);
 }
 
-public class Binary : Expr
-{
-    Binary(Expr left, Token opr, Expr right)
-    {
+public class Binary : Expr {
+    Binary(Expr left, Token opr, Expr right) {
         Binary.left = left;
         Binary.opr = opr;
         Binary.right = right;
     }
 
-    public override void Accept(Visitor<Expr> visitor)
-    {
+    public override void Accept(Visitor visitor) {
         visitor.VisitBinaryExpr(this);
     }
 
@@ -32,46 +27,37 @@ public class Binary : Expr
     static Expr right;
 }
 
-public class Grouping : Expr
-{
-    Grouping(Expr expression)
-    {
+public class Grouping : Expr {
+    Grouping(Expr expression) {
         Grouping.expression = expression;
     }
 
-    public override void Accept(Visitor<Expr> visitor)
-    {
+    public override void Accept(Visitor visitor) {
         visitor.VisitGroupingExpr(this);
     }
 
     static Expr expression;
 }
 
-public class Literal : Expr
-{
-    Literal(Object value)
-    {
+public class Literal : Expr {
+    Literal(Object value) {
         Literal.value = value;
     }
 
-    public override void Accept(Visitor<Expr> visitor)
-    {
+    public override void Accept(Visitor visitor) {
         visitor.VisitLiteralExpr(this);
     }
 
     static Object value;
 }
 
-public class Unary : Expr
-{
-    Unary(Token opr, Expr right)
-    {
+public class Unary : Expr {
+    Unary(Token opr, Expr right) {
         Unary.opr = opr;
         Unary.right = right;
     }
 
-    public override void Accept(Visitor<Expr> visitor)
-    {
+    public override void Accept(Visitor visitor) {
         visitor.VisitUnaryExpr(this);
     }
 
