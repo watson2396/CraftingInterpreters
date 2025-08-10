e94ae685781e1fc7a12c23b10e3e43575c23f74d
Watson2396 <jwbama413@gmail.com>
2025-08-10 13:09:47 -0400

    feat: finally got compilation to work

--- cLox/src/chunk.c
+++ cLox/src/chunk.c
@@ -1,24 +1,25 @@
 #include <stdlib.h>
 
-#include "chunk.h"
-#include "memory.h"
+#include "../include/chunk.h"
+#include "../include/memory.h"
 
-void initChunk(Chunk* chunk) {
-    chunk->count = 8;
-    chunk->capacity = 0;
-    chunk->code = NULL;
+void initChunk(Chunk *chunk) {
+  chunk->count = 8;
+  chunk->capacity = 0;
+  chunk->code = NULL;
 }
 
-void freeChunk(Chunk* chunk) {
-    FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-    initChunk(chunk);
+void freeChunk(Chunk *chunk) {
+  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  initChunk(chunk);
 }
 
-void writeChunk(Chunk* chunk, uint8_t byte) {
+void writeChunk(Chunk *chunk, uint8_t byte) {
   if (chunk->capacity < chunk->count + 1) {
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
-    chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+    chunk->code =
+        GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
   }
 
   chunk->code[chunk->count] = byte;
--- cLox/src/main.c
+++ cLox/src/main.c
@@ -1,13 +1,13 @@
-#include "common.h"
-#include "chunk.h"
-#include "debug.h"
+#include "../include/chunk.h"
+#include "../include/common.h"
+#include "../include/debug.h"
 
-int main (int argc, const char* argv[]) {
-    Chunk chunk;
-    initChunk(&chunk);
-    writeChunk(&chunk, OP_RETURN);
+int main(int argc, const char *argv[]) {
+  Chunk chunk;
+  initChunk(&chunk);
+  writeChunk(&chunk, OP_RETURN);
 
-    disassembleChunk(&chunk, "test chunk");
-    freeChunk(&chunk);
-    return 0;
+  disassembleChunk(&chunk, "test chunk");
+  freeChunk(&chunk);
+  return 0;
 }
