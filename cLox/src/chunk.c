4beac8efff3cc56b66f55ed816bfa0597ba9c9bc
Watson2396 <jwbama413@gmail.com>
2025-08-24 13:51:14 -0400

    changes: ch14; todo: run-length-encoding

--- cLox/src/chunk.c
+++ cLox/src/chunk.c
@@ -2,26 +2,42 @@
 
 #include "../include/chunk.h"
 #include "../include/memory.h"
+#include "../include/value.h"
 
-void initChunk(Chunk *chunk) {
+void initChunk(Chunk *chunk) 
+{
   chunk->count = 8;
   chunk->capacity = 0;
   chunk->code = NULL;
+  chunk->lines = NULL;
+  initValueArray(&chunk->constants);
 }
 
-void freeChunk(Chunk *chunk) {
-  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-  initChunk(chunk);
+void freeChunk(Chunk *chunk)
+{
+	FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+	FREE_ARRAY(int, chunk->lines, chunk->capacity);
+	initValueArray(&chunk->constants);
+	initChunk(chunk);
 }
 
-void writeChunk(Chunk *chunk, uint8_t byte) {
-  if (chunk->capacity < chunk->count + 1) {
+void writeChunk(Chunk *chunk, uint8_t byte, int lines) 
+{
+  if (chunk->capacity < chunk->count + 1) 
+  {
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
-    chunk->code =
-        GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+    chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
   }
 
   chunk->code[chunk->count] = byte;
+  chunk->code[chunk->count] = lines;
   chunk->count++;
+}
+
+int addConstant(Chunk* chunk, Value value)
+{
+  writeValueArray(&chunk->constants, value);
+  return chunk->constants.count - 1;
 }
--- cLox/src/memory.c
+++ cLox/src/memory.c
@@ -2,8 +2,10 @@
 
 #include "../include/memory.h"
 
-void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
-  if (newSize == 0) {
+void* reallocate(void *pointer, size_t oldSize, size_t newSize) 
+{
+  if (newSize == 0) 
+  {
     free(pointer);
     return NULL;
   }
